7024c2005452851e7491d5b1d11b6677b897f173
Sakshamsingh3251 <2005sakshamsingh@gmail.com>
2026-03-18 19:29:43 +0530

    Implement method to remove trailing zeros from string

---
+++ LC2710. Remove Trailing Zeros From a String.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String removeTrailingZeros(String num) {
+        int j= num.length()-1;
+        while(num.charAt(j) =='0'){
+            j--;
+        }
+        return num.substring(0,j+1);
+    }
+}
