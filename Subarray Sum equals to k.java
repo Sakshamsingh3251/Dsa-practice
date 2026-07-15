5337a3331da7faff391ef76a763531c2d8eab1f5
Saksham Singh <2005sakshamsingh@gmail.com>
2026-07-15 11:19:16 +0530

    Subarray sum equals to k *********
    Using Prefix Sum and HashMap

---
+++ Subarray Sum equals to k.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int result = 0;
+        int prefixsum = 0;
+
+        map.put(0,1);
+
+        for(int i = 0 ;i< nums.length ; i++){
+            prefixsum += nums[i];
+
+            if(map.containsKey(prefixsum - k)){
+                result += map.get(prefixsum - k);
+            }
+            map.put(prefixsum, map.getOrDefault(prefixsum, 0) + 1);
+        }
+        return result;
+
+    }
+}
