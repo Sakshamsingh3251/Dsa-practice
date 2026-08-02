bba46fad0973cf730369c67dada11a710c5e0b2a
Saksham Singh <2005sakshamsingh@gmail.com>
2026-08-02 18:10:39 +0530

    Lc.2390
    Implement a method to remove stars from a string by using a stack to manage characters.

---
+++ Remove Star from a String.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            if(ch == '*'){
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+            }else{
+                st.push(ch);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for(char ch : st){
+            result.append(ch);
+        }
+        return result.toString();
+    }
+
+}
