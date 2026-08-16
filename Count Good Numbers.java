4be609549be2d38ade0304d0ef725f0b5889adf6
Saksham Singh <2005sakshamsingh@gmail.com>
2026-08-16 19:27:13 +0530

    Count Good Numbers
    Using Recursion and Binary Exponentiation

---
+++ Count Good Numbers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static final int M = 1000000007;
+
+    public long findPower(long a, long b) {
+        if (b == 0) {
+            return 1;
+        }
+        long half = findPower(a, b / 2);
+        long result = (half * half) % M;
+        if (b % 2 == 1) {
+            result = (result * a) % M;
+        }
+        return result;
+    }
+
+    public int countGoodNumbers(long n) {
+        return (int) ((findPower(5, (n + 1) / 2) * findPower(4, n / 2)) % M);
+    }
+}
