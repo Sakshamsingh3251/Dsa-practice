e4bd0acf8587cb412e69cf3545dd753a177de05c
Saksham Singh <2005sakshamsingh@gmail.com>
2026-09-14 19:09:05 +0530

    LC.125>ValidPalindrome

---
+++ LC125.ValidPalindrome.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        // s = s.toLowerCase().replaceAll("[^A-Za-z0-9]","");
+        int i = 0;
+        int j = s.length()-1;
+        s = s.toLowerCase();
+        while( i <= j){
+            char ch1 = s.charAt(i);
+            char ch2 = s.charAt(j);
+
+            if(!(ch1 >= 'a' && ch1 <= 'z' || ch1 >= '0' && ch1 <= '9' )){
+                i++;
+                continue;
+
+            }
+            if(!(ch2 >= 'a' && ch2 <= 'z' || ch2 >= '0' && ch2 <= '9' )){
+                j--;
+                continue;
+
+            }
+            if(ch1 != ch2){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+        
+
+
+    }
+}
